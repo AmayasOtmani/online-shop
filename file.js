b2a3281b35c49ea1b7d859c32c706d7e5676591a
AmayasOtm <88903019+AmayasOtm@users.noreply.github.com>
2023-07-29 15:52:31 +0100

    updated the project

--- file.js
+++ file.js
@@ -1,12 +1,17 @@
 const express = require('express')
-const prodRouter = require('./prodRouter.js')
+const prodRouter = require('./rooters/prodRouter.js')
 const app = express()
 
 app.use(express.static('./'))
+app.set("view engine","ejs")
 app.use("/products",prodRouter)
 
 app.get("/",(req,res)=>
 {
-   console.log("this is working!")
+   res.send("Hi")
+})
+app.get("/*",(req,res)=>
+{
+   res.send("404 Error : page not found")
 })
 app.listen(4000)
